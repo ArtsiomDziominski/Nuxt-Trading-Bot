0331119f7679f4c77765a1af9d1f3c1bb5347913
ArtemDeminski <artem.d@konomic.com>
2024-10-01 21:57:41 +0400

    add button

--- components/layout/header/Header.vue
+++ components/layout/header/Header.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useTheme } from 'vuetify';
 import { localStorageKeyTheme, themeDark, themeLight } from '~/const/theme';
+import HeaderButtons from "~/components/layout/header/HeaderButtons.vue";
 
 const theme = useTheme();
 function toggleTheme() {
@@ -15,13 +16,34 @@ function toggleTheme() {
 			<v-icon icon="$vuetify" />
 		</template>
 
-		<v-app-bar-title>Application Bar</v-app-bar-title>
-		<v-btn @click="toggleTheme">
-			{{ theme.global.current.value.dark ? 'light' : 'dark' }}
-		</v-btn>
+		<v-app-bar-title>
+      <div class="title">
+        Application Bar
+        <div class="buttons">
+          <header-buttons />
+        </div>
+      </div>
+    </v-app-bar-title>
+    <template v-slot:append>
+      <v-btn @click="toggleTheme">
+        {{ theme.global.current.value.dark ? 'light' : 'dark' }}
+      </v-btn>
+    </template>
 	</v-app-bar>
 </template>
 
 <style scoped lang="scss">
+.title {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  .buttons {
+    display: flex;
+    flex-direction: row;
+    margin-left: 100px;
+    gap: 20px;
+  }
+}
 
 </style>
---
+++ components/layout/header/HeaderButtons.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const buttons = [
+  {
+    title: 'Home',
+    to: '/'
+  },
+  {
+    title: 'Bots',
+    to: '/bots'
+  }
+]
+</script>
+
+<template>
+  <v-btn v-for="button in buttons" variant="outlined" :to="button.to">
+    {{ button.title }}
+  </v-btn>
+</template>
+
+<style scoped lang="scss">
+
+</style>
