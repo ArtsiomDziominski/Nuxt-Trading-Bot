7b72b18125b498c4a79682c7583c47116e5d968f
ArtemDeminski <artem.d@konomic.com>
2024-10-13 23:50:54 +0400

    fix footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,22 +18,12 @@
 .app {
   max-width: 1920px;
   align-self: center;
-  display: grid;
-  grid-template-rows: 64px calc( 100vh - 64px) min-content;
-  grid-template-columns: 100%;
-  margin: 0 30px;
-
-  .header {
-    grid-area: 1/2/1/2;
-  }
+  display: flex;
+  flex-direction: column;
+  margin: 64px 30px 0 30px;
 
   .page {
     margin: 50px 0 50px 0;
-    grid-area: 2/1/3/2;
-  }
-
-  .footer {
-    grid-area: 3/1/4/2;
   }
 }
 </style>
