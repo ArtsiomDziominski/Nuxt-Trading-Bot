bf8fece5199703d0d28f598683e98888b421edc5
ArtemDeminski <artem.d@konomic.com>
2024-10-27 23:09:57 +0400

    set locale

--- plugins/i18n.config.ts
+++ plugins/i18n.config.ts
@@ -1,13 +1,19 @@
 const messages = {};
-const modules = import.meta.glob('../locales/*.json', { eager: true });
+const modules: Record<string, string> = import.meta.glob('../locales/*.json', { eager: true });
 
 for (const path in modules) {
 	const locale = path.match(/([a-z0-9]+)\.json$/i)[1];
 	messages[locale] = modules[path].default || modules[path];
 }
 
+function getLocaleCode(): string {
+	if (!import.meta.client) return 'en';
+	const localStorageLocal = localStorage.getItem('local');
+	return localStorageLocal || navigator.language.split('-')?.[0] || 'en';
+}
+
 export default defineI18nConfig(() => ({
+	locale: getLocaleCode(),
 	legacy: false,
-	locale: 'en',
 	messages: messages,
 }));
