66615a6c6de9ecaf207e8f8a90818d9cbb265b51
ArtemDeminski <artem.d@konomic.com>
2024-11-11 01:17:37 +0400

    fix api key

--- utils/encrypt.ts
+++ utils/encrypt.ts
@@ -1,9 +1,9 @@
-import CryptoJS from "crypto-js";
+import CryptoJS from 'crypto-js';
 
 export const encryptPassword = (password: string): string => {
 	return CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex);
 };
 
 export const encryptAPI = (value: string): string => {
-	return CryptoJS.AES.encrypt(value, useRuntimeConfig().public.API_URL).toString();
+	return CryptoJS.AES.encrypt(value, useRuntimeConfig().public.SECRET_KEY_API).toString();
 };
