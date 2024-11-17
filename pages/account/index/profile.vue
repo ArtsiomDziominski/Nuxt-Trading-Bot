8b318bb515e11e3231268260098d5f1ad9ea5214
ArtemDeminski <artem.d@konomic.com>
2024-11-17 11:22:22 +0400

    add profile

---
+++ pages/account/index/profile.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div class="profile">
+		<v-card />
+		<AccountProfile />
+		<v-card />
+	</div>
+</template>
+
+<style scoped lang="scss">
+.profile {
+  display: grid;
+  grid-template-columns: 0.8fr 1.4fr 0.8fr;
+  gap: 20px;
+  @media (max-width: 1024px) {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
